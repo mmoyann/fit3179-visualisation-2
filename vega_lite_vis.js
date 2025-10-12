78c1c824af4a3d40763868a1392c26eb6e00093a
Guoxi Lu <gguoluu@gmail.com>
2025-10-12 17:54:32 +1100

    Update vega_lite_vis.js

--- vega_lite_vis.js
+++ vega_lite_vis.js
@@ -1,3 +1,5 @@
 var spec = "map.vg.json";
 vegaEmbed("#map", spec, {actions: {export: true, source: true, editor: true}})
   .catch(console.error);
+vegaEmbed("#anomalies", "anomalies.vg.json", {actions: {export: true, source: true, editor: true}})
+  .catch(console.error);
