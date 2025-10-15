1d353f14513fd754bc7e0c99bdb7453d5b1fbf23
Guoxi Lu <gguoluu@gmail.com>
2025-10-15 17:27:15 +1100

    Delete vega_lite_vis.js

--- vega_lite_vis.js
+++
@@ -1,9 +0,0 @@
-var spec = "map.vg.json";  // Week 9 map file
-vegaEmbed("#mammal_map", spec, {actions: {export: true, source: true, editor: true}})
-  .then(() => console.log("✅ Mammal map loaded"))
-  .catch(console.error);
-
-var spec2 = "anomalies.vg.json";
-vegaEmbed("#anomalies", spec2, {actions: {export: true, source: true, editor: true}})
-  .then(() => console.log("✅ Temperature anomalies chart loaded"))
-  .catch(console.error);
